53956c61499b715001435405f3b11f0da2417988
Juliana Gutierrez <alejuli.0627@gmail.com>
2024-09-26 15:16:58 -0500

    feat: add GET endpoints to FastAPI application

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-# import random
+from fastapi import FastAPI
+
 from components import MenuItemRepository, MealGenerator
 
-folder_path = "C:\\Users\\Juli\\Desktop\\Juliana\\Programacion\\study_plan_practice\\first_week\\final_project_1\\folder_path"
+app = FastAPI()
+
+folder_path = "C:\\Users\\Juli\\Desktop\\Juliana\\Programacion\\repositories\\Meal-Generator\\folder_path"
 repository = MenuItemRepository(folder_path)
 
 meal_generator = MealGenerator(repository)
-meal = meal_generator.generate_meal(with_extra=True, with_soup=True, with_meat=True)
-message = meal.to_string()
-print(message)
-print("finishhh")
+meal = meal_generator.generate_meal(with_extra=False,
+                                    with_soup=True,
+                                    with_meat=False)
+result = meal.to_string()
+print(result)
 
 
-# WEEK_DAYS = [menu_del_dia.get_meal(False, False, False)]
+# @app.get("/{component}")
+# async def get_components_meal(component):
 
-# for i in range(0, 4):
-#     WEEK_DAYS.append(menu_del_dia.get_meal(False, False, True))
+#     file = open(f"folder_path\\{component}.json")
+#     return json.load(file)
 
-# random.shuffle(WEEK_DAYS)
 
-# meal1 = menu_del_dia.get_meal(False, True, True)
-# print("End")
+@app.get("/{with_extra}/{with_soup}/{with_meat}")
+async def get_menu(with_extra: bool, with_soup: bool, with_meat: bool):
+
+    folder_path = "C:\\Users\\Juli\\Desktop\\Juliana\\Programacion\\repositories\\Meal-Generator\\folder_path"
+    repository = MenuItemRepository(folder_path)
+
+    meal_generator = MealGenerator(repository)
+    meal = meal_generator.generate_meal(with_extra, with_soup, with_meat)
+    return meal.to_string()
